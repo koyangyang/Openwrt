f1e24de49ff1c33be630ac7d5bb0cbeece6ff641
koyangyang <codedevyang@qq.com>
2021-02-17 23:48:13 +0800

    add godproxy

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.0.1/g' package/base-files/files/bin/config_generate
+
+# add packages
+git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
